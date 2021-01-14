ca34a501d22fc6d2e20973a13b2ba61386598e98
waxie100 <77423967+waxie100@users.noreply.github.com>
2021-01-14 11:42:41 +0800

    Update index.js

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -12,16 +12,11 @@ ReactDOM.render(
     <HelmetProvider>
       <ProductsContextProvider>
         <CartContextProvider>
-  
             <Routes />
-   
         </CartContextProvider>
       </ProductsContextProvider>
     </HelmetProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
