7050b1b0e910f9a84dc457bf832774d4a68ecf57
waxie100 <77423967+waxie100@users.noreply.github.com>
2021-01-14 11:41:35 +0800

    Update footer.js

--- shopping-cart/src/components/shared/footer.js
+++ shopping-cart/src/components/shared/footer.js
@@ -4,7 +4,7 @@ import styles from './footer.module.scss';
 const Footer = () => {
     return ( 
         <footer className={`${styles.footer}  mt-5 p-3`}>
-            2019 &copy; Luke Towner 
+            &copy; Luke Towner 
         </footer>
      );
 }
